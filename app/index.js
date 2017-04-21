5111a0bc22003a718797d1b8bee625453b4bf992
GHJ <w124543238@126.com>
2017-04-21 14:57:10 +0800

    change

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
+import $ from 'jquery'
 function component () {
-  var element = document.createElement('div');
-
+//   var element = document.createElement('div');
+    var element=$('<div></div>')
   /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-  return element;
+//   element.innerHTML = _.join(['Hello','webpack'], ' ');
+element.html(_.join(['Hello','webpack'],'~'))
+  return element.get(0);
 }
 
 document.body.appendChild(component());
