af8426199cd1fa51c08ab4c148865d685e5f6d99
GHJ <w124543238@126.com>
2017-05-23 19:41:42 +0800

    App传一个函数给TodoInput

--- hello/src/TodoInput.js
+++ hello/src/TodoInput.js
@@ -2,6 +2,12 @@ import React,{Component} from 'react';
 
 export default class TodoInput extends Component {
     render(){
-        return <input type="text" defaultValue={this.props.content}/>
+        return <input type="text" defaultValue={this.props.content}
+         onKeyPress={this.submit}/>
+    }
+    submit(e){
+        if(e.key === 'Enter') {
+            this.props.onSubmit.call()
+        }
     }
 }
