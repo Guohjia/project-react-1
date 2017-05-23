55d700eded13231be9cdd3155f3792514ed65d9d
GHJ <w124543238@126.com>
2017-05-23 19:17:13 +0800

    fix value

--- hello/src/App.js
+++ hello/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state={
-      newTodo:'test',
+      newTodo:'',
       todoList:[
         {id:1,title:'第一个待办'},
         {id:2,title:'第二个待办'}
