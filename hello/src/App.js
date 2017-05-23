16536c92731f33f484ed7016afd7e188b8395cc2
GHJ <w124543238@126.com>
2017-05-23 20:12:40 +0800

    新增todo fix value again

--- hello/src/App.js
+++ hello/src/App.js
@@ -11,8 +11,6 @@ class App extends Component {
     this.state={
       newTodo:'',
       todoList:[
-        {id:1,title:'第一个待办'},
-        {id:2,title:'第二个待办'}
       ]
     }
   }
@@ -20,7 +18,7 @@ class App extends Component {
   render() {
     let todos=this.state.todoList.map((item,index)=>{
       return (
-        <li>
+        <li key={index}>
           <TodoItem todo={item} />
         </li>
       )
@@ -30,7 +28,9 @@ class App extends Component {
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo}/>
+          <TodoInput content={this.state.newTodo}
+          onChange={this.changeTitle.bind(this)}
+          onSubmit={this.addTodo.bind(this)}/>
         </div>
         <ol>
           {todos}
@@ -38,11 +38,31 @@ class App extends Component {
       </div>
     )
   }
-  addTodo(){
-    console.log('我得添加一个todo了')
+  changeTitle(event){
+    this.setState({
+      newTodo:event.target.value,
+      todoList:this.state.todoList
+    })
+  }
+  addTodo(event){
+    this.state.todoList.push({
+      id:idMaker(),
+      title:event.target.value,
+      status:null,
+      deleted:false
+    })
+    this.setState()({
+      newTodo:'',
+      todoList:this.state.todoList
+    })
   }
 }
 
 export default App;
 
+let id=0;
 
+function idMaker(){
+  id+=1
+  return id
+}
