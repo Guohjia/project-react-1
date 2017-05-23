ef444d601af512c7a90731a98e4ec52072192d7b
GHJ <w124543238@126.com>
2017-05-23 20:34:15 +0800

    标记已完成/未完成

--- hello/src/App.js
+++ hello/src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
     let todos=this.state.todoList.map((item,index)=>{
       return (
         <li key={index}>
-          <TodoItem todo={item} />
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)}/>
         </li>
       )
     })
@@ -37,6 +37,10 @@ class App extends Component {
         </ol>
       </div>
     )
+  }
+  toggel(e,todo){
+    todo.status=todo.status==='completed'?'':'completed'
+    this.setState(this.state)
   }
   changeTitle(event){
     this.setState({
--- hello/src/TodoInput.js
+++ hello/src/TodoInput.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 
 export default class TodoInput extends Component {
     render(){
-        return <input type="text" Value={this.props.content}
+        return <input type="text" value={this.props.content}
          onChange={this.changeTitle.bind(this)}
          onKeyPress={this.submit.bind(this)}/>
     }
