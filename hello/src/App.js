045f0ef5b9ab281950560acae95291ea3dc06ff2
GHJ <w124543238@126.com>
2017-05-24 20:22:38 +0800

    更新Todoltem样式，改正拼写错误

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import './reset.css';
 import TodoInput from './TodoInput';
@@ -6,31 +6,32 @@ import TodoItem from './TodoItem';
 import 'normalize.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-      newTodo:'',
-      todoList:[
+    this.state = {
+      newTodo: '',
+      todoList: [
       ]
     }
   }
-
   render() {
-    let todos=this.state.todoList.filter((item)=>!item.deleted).map((item,index)=>{
+    let todos = this.state.todoList
+      .filter((item) => !item.deleted)
+      .map((item, index) => {
         return (
-        <li key={index}>
-          <TodoItem todo={item} onToggle={this.toggle.bind(this)}
-              onDelete={this.delete.bind(this)}/>
-        </li>
-      )
-    })      
+          <li key={index} >
+            <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+              onDelete={this.delete.bind(this)} />
+          </li>
+        )
+      })
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
           <TodoInput content={this.state.newTodo}
-          onChange={this.changeTitle.bind(this)}
-          onSubmit={this.addTodo.bind(this)}/>
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol className="todoList">
           {todos}
@@ -38,38 +39,39 @@ class App extends Component {
       </div>
     )
   }
-  toggel(e,todo){
-    todo.status=todo.status==='completed'?'':'completed'
+
+  toggle(e, todo) {
+    todo.status = todo.status === 'completed' ? '' : 'completed'
     this.setState(this.state)
   }
-  changeTitle(event){
+  changeTitle(event) {
     this.setState({
-      newTodo:event.target.value,
-      todoList:this.state.todoList
+      newTodo: event.target.value,
+      todoList: this.state.todoList
     })
   }
-  addTodo(event){
+  addTodo(event) {
     this.state.todoList.push({
-      id:idMaker(),
-      title:event.target.value,
-      status:null,
-      deleted:false
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
     })
-    this.setState()({
-      newTodo:'',
-      todoList:this.state.todoList
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
     })
   }
-  delete(event,todo){
-    todo.deleted=true
+  delete(event, todo) {
+    todo.deleted = true
     this.setState(this.state)
   }
 }
 export default App;
 
-let id=0;
+let id = 0;
 
-function idMaker(){
-  id+=1
+function idMaker() {
+  id += 1
   return id
 }
--- hello/src/TodoItem.js
+++ hello/src/TodoItem.js
@@ -1,19 +1,20 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import './TodoItem.css'
 export default class TodoItem extends Component {
-    render(){
+    render() {
         return (
             <div className="TodoItem">
-                <input type="checkbox" checked={this.props.todo.status==='completed'}
-                 onChange={this.toggle.bind(this)}/>
-                 <span className="title">{this.props.todo.title}</span>
-                 <button onClick ={this.delete.bind(this)}>删除</button>
+                <input type="checkbox" checked={this.props.todo.status === 'completed'}
+                    onChange={this.toggle.bind(this)} />
+                <span className="title">{this.props.todo.title}</span>
+                <button onClick={this.delete.bind(this)}>删除</button>
             </div>
         )
     }
-    toggle(e){
-        this.props.onToggle(e,this.props.todo)
+    toggle(e) {
+        this.props.onToggle(e, this.props.todo)
     }
-    delete(e){
-        this.props.onDelete(e,this.props.todo)
+    delete(e) {
+        this.props.onDelete(e, this.props.todo)
     }
 }
