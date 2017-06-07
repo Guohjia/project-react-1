735e118fa68a602d2bf847b9a63cdb1c86eb0cc8
GHJ <w124543238@126.com>
2017-06-07 17:17:59 +0800

    注册和登录表单间切换

--- hello/src/UserDialog.js
+++ hello/src/UserDialog.js
@@ -4,7 +4,7 @@ export default class UserDialog extends Component {
     constructor(props){
         super(props)
         this.state={
-            selected:'signup'
+            selected:'signIn'
         }
     }
     switch(e) {
@@ -13,15 +13,8 @@ export default class UserDialog extends Component {
         })
     }
     render(){
-        return (
-            <div className="UserDialog-Wrapper">
-                <div className="UserDialog">
-                    <nav onChange={this.switch.bind(this)}>  {/*事件代理，给父元素绑定事件*/}
-                        <label><input type="radio" value="signIn" checked={this.selected==='signIn'}/>登录</label>
-                        <label><input type="radio" value="signUp" checked={this.selected==='signUp'}/>注册</label>
-                    </nav>
-                    <div className="panes">
-                        <form action="" className="signUp">  
+        let signUpForm=(
+            <form action="" className="signUp">  
                             {/*注册*/}
                             <div className="row">
                                 <label>用户名</label>
@@ -31,11 +24,13 @@ export default class UserDialog extends Component {
                                 <label>密码</label>
                                 <input type="password"/>
                             </div>
-                            <div className="row">
-                                <button type="submit">登录</button>
+                            <div className="row actions">
+                                <button type="submit">注册</button>
                             </div>
                         </form>
-                        <form action="" className="signIn">
+        )
+        let signInForm=(
+            <form action="" className="signIn">
                             {/*登录*/}
                             <div className="row">
                                  <label>用户名</label>
@@ -45,10 +40,21 @@ export default class UserDialog extends Component {
                                 <label>密码</label>
                                 <input type="password"/>
                             </div>
-                            <div className="row">
-                                <button type="submit">注册</button>
+                            <div className="row actions">
+                                <button type="submit">登录</button>
                             </div>
                         </form>
+        )
+        return (
+            <div className="UserDialog-Wrapper">
+                <div className="UserDialog">
+                    <nav onChange={this.switch.bind(this)}>  {/*事件代理，给父元素绑定事件*/}
+                        <label><input type="radio" value="signIn" checked={this.state.selected==='signIn'}/>登录</label>{/*checked可以让单选按钮有一个预选值   */}
+                        <label><input type="radio" value="signUp" checked={this.state.selected==='signUp'}/>注册</label>
+                    </nav>
+                    <div className="panes">
+                        {this.state.selected==='signUp' ?signUpForm:null}
+                        {this.state.selected==='signIn' ?signInForm:null}
                     </div>
                 </div>
             </div>
