ffa51da7d4097f429aff223609b77c6aa8d6daed
GHJ <w124543238@126.com>
2017-06-08 17:30:01 +0800

    提示友好的报错

--- hello/src/UserDialog.js
+++ hello/src/UserDialog.js
@@ -25,7 +25,14 @@ export default class UserDialog extends Component {
             this.props.onSignUp(user)
         }
         let error=(error)=>{
-            alert(error)
+            switch(error.code){
+                case 202:
+                    alert('用户名已经占用')
+                    break
+                default:
+                alert(error)
+                break
+            }
         }
         signUp(username,password,success,error)
     }
@@ -36,7 +43,14 @@ export default class UserDialog extends Component {
             this.props.onSignIn(user)
         }
         let error=(error)=>{
-            alert(error)
+           switch(error.code){
+                case 210:
+                    alert('用户名与密码不匹配')
+                    break
+                default:
+                alert(error)
+                break
+            }
         }
         signIn(username,password,success,error)
     }
