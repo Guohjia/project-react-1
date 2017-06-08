a5bd3d91668513b871b75bab35d99a7ea519a7bf
GHJ <w124543238@126.com>
2017-06-08 16:30:26 +0800

    添加登出功能

--- hello/src/leanCloud.js
+++ hello/src/leanCloud.js
@@ -32,6 +32,12 @@ export function getCurrentUser(){
     return null
   }
 }
+
+export function signOut(){
+  AV.User.logOut()
+  return undefined
+}
+
 function getUserFromAVUser(AVUser){
   return {
     id:AVUser.id,
