5e6efd3f21183586f54131d5562691df258be8f9
GHJ <w124543238@126.com>
2017-07-12 14:08:48 +0800

    add date

--- hello/src/App.js
+++ hello/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     this.state = {
       user: getCurrentUser() || {},
       newTodo: '',
-      changedTodo:'',
+      changedTodo: '',
       todoList: []     //每次进入页面的时候load
     }
     this.JSONCopy = this.JSONCopy.bind(this)
@@ -37,15 +37,23 @@ class App extends Component {
           <li key={index} >
             <TodoItem todo={item} onToggle={this.toggle.bind(this)}
               onDelete={this.delete.bind(this)} onChange={this.changeTodo.bind(this)}
-               onBlur={this.sendData.bind(this)}/>
+              onBlur={this.sendData.bind(this)} />
           </li>
         )
       })
     return (
       <div className="App">
-        <h1>{this.state.user.username || '我'}的待办
-          {this.state.user.id ? <button onClick={this.signOut.bind(this)}>登出</button> : null}
-        </h1>
+        <div className="date">
+          <h1>{this.getDate().day}
+          </h1>
+          <div className="monthYear">
+            <h4>{this.getDate().month}</h4>
+            <span>{this.getDate().year}</span>
+          </div>
+          <h2 className="week">
+            {this.getDate().week}
+          </h2>
+        </div>
         <div className="inputWrapper">
           <TodoInput content={this.state.newTodo}
             onChange={this.changeTitle.bind(this)}
@@ -54,23 +62,35 @@ class App extends Component {
         <ol className="todoList">
           {todos}
         </ol>
+        {this.state.user.id ? <button onClick={this.signOut.bind(this)}>登出</button> : null}
         {this.state.user.id ? null : <UserDialog onSignUp={this.onSignUpOronSignIn.bind(this)} onSignIn={this.onSignUpOronSignIn.bind(this)} />}
       </div>
     )
+  }
+
+  getDate() {
+    let currentDate = new Date()
+    let monthArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov.', 'Dec']
+    let weekArray = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
+    let dateObj = {
+      year: currentDate.getFullYear(), month: monthArray[currentDate.getMonth()],
+      day: currentDate.getDate(), week: weekArray[currentDate.getDay() - 1]
+    }
+    return dateObj
   }
   signOut() {
     signOut();  //这里的signOut,todolistStore是从leanCloud导入的LeanCloud,每次退出的时候上传todolist到数据库
     let stateCopy = this.JSONCopy(this.state)
     stateCopy.user = {}
-    stateCopy.todoList=[] //登出todoList清空
+    stateCopy.todoList = [] //登出todoList清空
     this.setState(stateCopy)
   }
-  
+
   onSignUpOronSignIn(user) {
     let stateCopy = this.JSONCopy(this.state)  //块级作用域必须为函数作用域，才能让getByUser中的stateCopy起到作用
     stateCopy.user = user
     TodoModel.getByUser(user, (todos) => {
-      if(todos.length!==0){
+      if (todos.length !== 0) {
         stateCopy.todoList = todos
       }
       this.setState(stateCopy)  //this.setState必须在这里，保证其在获取todos之后
@@ -87,26 +107,25 @@ class App extends Component {
     //componentDidUpdate 会在组件更新[数据更新]之后调用。可以把 localStore.save('todoList', this.state.todoList) 写在这个钩子里。当用户的待办事项发生改变之后，即存储操作
   }
 
-  changeTodo(event,todo){
-    todo.title=event.target.value
-    let stateCopy = this.JSONCopy(this.state) 
-    stateCopy.changedTodo=todo //保存改变的todo
-    this.setState(stateCopy) 
+  changeTodo(event, todo) {
+    todo.title = event.target.value
+    let stateCopy = this.JSONCopy(this.state)
+    stateCopy.changedTodo = todo //保存改变的todo
+    this.setState(stateCopy)
   }
 
-  sendData(todo){
-    let oldState=this.JSONCopy(this.state)  //保留更新之前的state
-    oldState.changedTodo=''  //不管是否发送请求成功 changedTodo都将清空
-    let changedTodo=this.state.changedTodo  //获得要更新的todo
+  sendData(todo) {
+    let oldState = this.JSONCopy(this.state)  //保留更新之前的state
+    oldState.changedTodo = ''  //不管是否发送请求成功 changedTodo都将清空
+    let changedTodo = this.state.changedTodo  //获得要更新的todo
     TodoModel.update(changedTodo, () => {
-       this.setState(this.state)
+      this.setState(this.state)
     }, (error) => {
       this.setState(oldState)  //请求失败 更新之前的state
     })
   }
 
   toggle(e, todo) {
-    console.log(e.target)
     let oldStatus = todo.status
     todo.status = todo.status === 'completed' ? '' : 'completed'
     TodoModel.update(todo, () => {
--- hello/src/TodoItem.js
+++ hello/src/TodoItem.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './TodoItem.css'
-import {TodoModel } from './leanCloud.js'
 export default class TodoItem extends Component {
     render() {
         return (
@@ -8,15 +7,23 @@ export default class TodoItem extends Component {
                 <input type="checkbox" checked={this.props.todo.status === 'completed'}
                     onChange={this.toggle.bind(this)} />
                 <input type="text" className="title" value={this.props.todo.title} onChange={this.changeTodo.bind(this)}
-                    onBlur={this.props.onBlur}/> 
-                {/*<span>{this.props.todo.title}</span> */} 
+                    onBlur={this.props.onBlur}
+                     />
+                {/*<span>{this.props.todo.title}</span> */}
                 {/*失去焦点的时候发送请求，生成新的todo */}
-                <button onClick={this.delete.bind(this)}>删除</button>
+                <span onClick={this.delete.bind(this)}>x</span>
             </div>
         )
     }
-    changeTodo(e){
-        this.props.onChange(e,this.props.todo)
+    // submit(props, e) {
+    //     if (e.key === 'Enter') {
+    //         if (e.target.value.trim() !== '') { //输出为空拒绝
+    //             props.onBlur()
+    //         }
+    //     }
+    // }
+    changeTodo(e) {
+        this.props.onChange(e, this.props.todo)
     }
     toggle(e) {
         this.props.onToggle(e, this.props.todo)
